7e3c81f8478243803322bc30dc2c77a0643250b0
Chris Oliver <excid3@gmail.com>
2012-07-14 13:25:56 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cultivate::Application.config.secret_token = '5012a20bba205bbe560cf7dacb3c467208152bcb0660e281643f693a64c7746367e55fe1a7f2ee0b018fee9980075eed2f9325fb1fb1d3152f14bc88ea01a621'
