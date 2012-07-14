ceae52a90cab3737664ab8b6b7104643db5c15d2
Chris Oliver <excid3@gmail.com>
2012-07-14 15:52:11 -0500

    Clean up a bit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120714183303) do
+ActiveRecord::Schema.define(:version => 20120714203620) do
 
   create_table "companies", :force => true do |t|
     t.string   "name"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(:version => 20120714183303) do
   end
 
   add_index "companies", ["slug"], :name => "index_companies_on_slug", :unique => true
+
+  create_table "goals", :force => true do |t|
+    t.integer  "company_id"
+    t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                                :default => "", :null => false
